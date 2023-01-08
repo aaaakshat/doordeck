c05abac7550c1a9e4601a90d93c81bdceaf46c2c
aaaakshat <33050725+aaaakshat@users.noreply.github.com>
2023-01-07 22:29:12 -0500

    Initial

---
+++ gen.py
@@ -0,0 +1,48 @@
+#!/usr/bin/env python3
+
+from PIL import ImageFont, Image, ImageDraw
+import csv
+
+all_names = []
+with open("names.csv") as csv_file:
+    reader = csv.reader(csv_file, delimiter=',')
+    line = 0
+    for row in reader:
+        if line == 0:
+            line += 1
+            pass
+        else:
+            all_names.append(row[0])
+
+for i in range(len(all_names) // 8 + 1):
+    names = all_names[8*i:8*i+8]
+    template = Image.open("template.png")
+    draw = ImageDraw.Draw(template)
+
+
+    font = ImageFont.truetype("Arial.ttf", 35)
+    x,y = (100, 540)
+
+    for name in names:
+        draw.text((x,y), name.upper(), font=font, fill=(0,0,0))
+        y += 685
+        if (y > 2595):
+            y = 540
+            x = 1150
+
+    x,y = (530, 500)
+    font2 = ImageFont.truetype("Breakable.ttf", 80)
+    for name in names:
+        txt = Image.new("L", (300, 130), 255)
+        d = ImageDraw.Draw(txt)
+        d.text((0,0), name.title(),font=font2, fill=0)
+        w = txt.rotate(2.1, expand=1, fillcolor=255)
+        template.paste(w, (x,y))
+        y += 685
+        if (y > 2595):
+            y = 500
+            x = 1570
+
+    title = "gen/page" + str(i+1) + ".png"
+    template.save(title)
+
